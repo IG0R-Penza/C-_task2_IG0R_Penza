45cb700e7715fb6fcb5140fd750f6b8d9a3f0c08
IG0R_Penza <igorkovalev2006@gmail.com>
2024-11-08 03:15:34 +0300

    2)inheritance

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Car mers = new Car("Mersedes", "B156TA77");
+Bicycle bmx = new Bicycle("WeThePeople", "chromium-molybdenum");
+mers.drive();
+bmx.ride();
+class Vehicle
+{
+    private string _name="";
+    public string Name { 
+        get { return _name; } 
+        set { _name = value; }
+    }
+    public Vehicle(string name) { Name = name; }
+}
+
+class Car : Vehicle
+{
+    private string _LicencePlate = "";
+    public string LicensePlate
+    {
+        get { return _LicencePlate; }
+        set { _LicencePlate = value; }
+    }
+    public Car(string name, string licenseplate)
+        : base(name)
+    {
+        LicensePlate = licenseplate;
+    }
+    public void drive()
+    {
+        Console.WriteLine($"Driving {this.Name}, {_LicencePlate}");
+    }
+}
+
+class Bicycle : Vehicle
+{
+    private string _FrameMaterial = "";
+    public string FrameMaterial
+    {
+        get { return _FrameMaterial; }
+        set { _FrameMaterial = value; }
+    }
+    public Bicycle(string name, string framematerial)
+        : base(name)
+    {
+        FrameMaterial = framematerial;
+    }
+    public void ride()
+    {
+        Console.WriteLine($"Riding {this.Name} with {_FrameMaterial} frame");
+    }
+}
--- task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
+++ task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aca6e0002bb337b3fe775b3dd228b669c327b3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
