a2eb9c836999667564ed18a3c20f54afd2f05a31
IG0R_Penza <igorkovalev2006@gmail.com>
2024-11-09 00:21:40 +0300

    single responsibility

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Employee alice = new Employee("Alice", "manager");
+Employee bob = new Employee("Bob", "engineer");
+Console.WriteLine(SalaryCounter.Salary(alice));
+Console.WriteLine(SalaryCounter.Salary(bob));
+class Employee
+{
+    public string _name = "";
+    public string _post = "";
+    public Employee(string name, string post) {  _name = name; _post = post;}
+}
+class SalaryCounter
+{
+    static public int mrot = 20000;
+    static public double Salary(Employee employee)
+    {
+        switch (employee._post) {
+            case "engineer":
+                return 4 * mrot;
+            case "manager":
+                return 4.5 * mrot;
+            case "cleaner":
+                return mrot;
+            case "boss":
+                return 20 * mrot;
+            default: return 0;
+        }
+    }
+}
