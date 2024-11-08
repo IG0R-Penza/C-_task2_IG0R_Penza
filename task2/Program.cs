f7cfc1808c0e8663c76b8efb20d82970055ebc0d
IG0R_Penza <igorkovalev2006@gmail.com>
2024-11-08 20:12:43 +0300

    3)polymorphism

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿{
+    Dog dog = new Dog("Sheepdog");
+    Cat cat = new Cat("Scottish fold");
+    dog.MakeSound();
+    cat.MakeSound();
+}
+
+class Animal
+{
+    private string _name="";
+    public string Name {
+        get { return _name; }
+        set { _name = value; }
+    }
+    public Animal(string name){Name = name;}
+    public virtual void MakeSound() { Console.WriteLine($"{_name}: SOUND"); }
+}
+
+class Dog : Animal
+{
+    public Dog(string name) : base(name) {}
+    public override void MakeSound() { Console.WriteLine($"{Name}: BARK!!!"); }
+}
+class Cat : Animal
+{
+    public Cat(string name) : base(name){}
+    public override void MakeSound() { Console.WriteLine($"{Name}: Meow"); }
+}
