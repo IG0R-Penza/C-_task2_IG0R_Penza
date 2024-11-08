e96bada60b4309537a7123aab59907bff27b613a
IG0R_Penza <igorkovalev2006@gmail.com>
2024-11-08 22:11:22 +0300

    4)abstraction

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿{
+    Rectangle pryamoug = new Rectangle();
+    pryamoug.Width = 3;
+    pryamoug.Height = 4;
+    Circle krug = new Circle();
+    krug.Radius = 3;
+    Console.WriteLine($"Площадь прямоугольника: {pryamoug.GetArea()}\nПлощадь круга: {krug.GetArea()}");
+}
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public override double GetArea()
+    {
+        return (Width * Height);
+    }
+}
+class Circle : Shape
+{
+    public double Radius { get; set; }
+    public override double GetArea()
+    {
+        return Radius*Radius*3.14;
+    }
+}
