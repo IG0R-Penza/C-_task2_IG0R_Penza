e052e4388c407088cf1b723556355ca5a44381be
IG0R_Penza <igorkovalev2006@gmail.com>
2024-11-10 17:09:46 +0300

    generic2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int[] NumArray = { 3, 4, 1 };
+FindMin<int> nums = new FindMin<int>(NumArray);
+int min = nums.Find();
+Console.WriteLine(min);
+class FindMin<T> where T : IComparable
+{
+    private T[] _values;
+    public FindMin(T[] values){_values=values; }
+    public T Find() { Array.Sort(_values);
+        return _values[0];
+    }
+}
