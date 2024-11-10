b07afab25e22d813b1b072c546846156e9ea34ec
IG0R_Penza <igorkovalev2006@gmail.com>
2024-11-10 04:21:33 +0300

    Interface Segregation

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System.Net.Http.Headers;
+
+{
+    MFU mfu = new MFU("Brother");
+    IPrinter printer = new Printer("HP");
+    mfu.Scan();
+    mfu.Print();
+    printer.Print();
+}
+
+interface IPrinter
+{
+    public void Print() { Console.WriteLine("Printing..."); }
+}
+interface IScanner
+{
+    public void Scan(string name) { Console.WriteLine("Scanning..."); }
+}
+class MFU : IPrinter, IScanner
+{
+    private string _name = "";
+    public MFU(string name) { _name = name; }
+    public void Scan() { Console.WriteLine("Scanning..."); }
+    public void Print() { Console.WriteLine("Printing..."); }
+}
+class Printer : IPrinter
+{
+    private string _name = "";
+    public Printer(string name) { _name = name; }
+}
