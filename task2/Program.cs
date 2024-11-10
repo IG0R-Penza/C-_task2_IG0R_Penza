d2973b3adcea052608400003e95cd9f24a8b90e3
IG0R_Penza <igorkovalev2006@gmail.com>
2024-11-10 20:26:33 +0300

    generic3

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int num1 = 1;
+int num2 = 2;
+Console.WriteLine($"before swap: {num1}, {num2}");
+Swapper<int>.Swap(ref num1,ref num2);
+Console.WriteLine($"after swap: {num1}, {num2}");
+
+static class Swapper<T>
+{
+    static public void Swap(ref T value1, ref T value2)
+    {
+        T temp = value1;
+        value1=value2;
+        value2=temp;
+    }
+}
--- task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
+++ task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3a7fe697b401a1931e3cf9e2862956d265f7753")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+978e6a71f90361f685ba63856a2a3525500bb608")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
