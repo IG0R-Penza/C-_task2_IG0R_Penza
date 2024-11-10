3e58d7690811cfdb25fc94483ba249770f46218a
IG0R_Penza <igorkovalev2006@gmail.com>
2024-11-10 20:56:01 +0300

    generic5

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿{
+    ListStorage<string> students_names = new ListStorage<string>();
+    students_names.Add("alice");
+    students_names.Add("bob");
+    students_names.Add("charlie");
+    Console.WriteLine(students_names.Get(0));
+    Console.WriteLine(students_names.Get(1));
+    Console.WriteLine(students_names.Get(2));
+}
+interface IStorage<T>
+{
+    public void Add(T element);
+    public T Get(int index);
+}
+class ListStorage<T> : IStorage<T>
+{
+    List<T> _list=new List<T>();
+    public void Add(T element){_list.Add(element);}
+    public T Get(int index) { return _list[index]; }
+}
