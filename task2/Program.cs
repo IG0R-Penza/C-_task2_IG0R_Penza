7d732cdff3910befd8a48c601897effdc0cf20e3
IG0R_Penza <igorkovalev2006@gmail.com>
2024-11-10 16:20:18 +0300

    Dependency Inversion

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿{
+    Logger logger1 = new Logger(new ConsoleLogger());
+    Logger logger2 = new Logger(new FileLogger());
+    logger1.CreateLog("Log1");
+    logger2.CreateLog("Log2");
+}
+
+interface ILogger
+{
+    public void Log(string message);
+}
+class ConsoleLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"ConsloleLog: {message}");
+    }
+}
+class FileLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"Log '{message}' written to file");
+    }
+}
+class Logger
+{
+    private ILogger _logger;
+    public Logger(ILogger logger) {  _logger = logger; }
+    public void CreateLog(string message)
+    {
+        _logger.Log(message);
+    }
+}
