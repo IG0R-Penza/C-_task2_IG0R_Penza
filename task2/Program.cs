91f226534c3abc7c5c250d26030c07f59da996d2
IG0R_Penza <igorkovalev2006@gmail.com>
2024-11-10 16:35:58 +0300

    generic1

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿{
+    Pair<string, int> pair = new Pair<string, int>("abc", 123);
+    pair.SetValueT("qwe");
+    Console.WriteLine($"{pair.GetValueT()} ,  {pair.GetValueU()}");
+}
+class Pair<T, U>
+{
+    private T _ValueT;
+    private U _ValueU;
+    public Pair(T ValueT, U ValueU) {  _ValueT = ValueT; _ValueU = ValueU; }
+    public void SetValueT(T ValueT) { _ValueT= ValueT; }
+    public void SetValueU(U ValueU) { _ValueU = ValueU; }
+    public T GetValueT() { return _ValueT; }
+    public U GetValueU() { return _ValueU; }
+}
