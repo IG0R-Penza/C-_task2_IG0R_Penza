3c799e204b589214763de3f1fbea2522d86fae38
IG0R_Penza <igorkovalev2006@gmail.com>
2024-11-10 03:19:05 +0300

    Liskov Substitution

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Bird filin = new Bird("Filin");
+Bird pingiun = new Pinguin("Pin");
+filin.Fly();
+pingiun.Fly();
+class Bird
+{
+    protected string _name = "";
+    public Bird(string name) { _name = name; }
+    public virtual void Fly() { Console.WriteLine($"{_name} is flying"); }
+}
+class Pinguin : Bird
+{
+    public Pinguin(string name) : base(name){}
+    public override void Fly() { Console.WriteLine($"{_name} can't fly 'cause it's pinguin"); }
+}
